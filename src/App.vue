843dc64d5349a391bd489706e2a3f125c2b6fcd3
Dan Cameron <dan@sproutventure.com>
2022-06-08 16:01:42 -0700

    Using Vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import DSPColumn from "./components/DigitalSensingProductsColumn.vue";</script>
+
+<template>
+
+	<main id="app-wrap" class="aspect-video bg-slate-50">
+		<div class="grid grid-flow-col auto-cols-max gap-4">
+			<DSPColumn/>
+			<DSPColumn/>
+			<div>02</div>
+			<div>03</div>
+			<div>04</div>
+			<div>05</div>
+			<div>06</div>
+			<div>07</div>
+
+		</div>
+	</main>
+
+
+</template>
+
+<style></style>
---
+++ src/components/DigitalSensingProductsColumn.vue
@@ -0,0 +1,91 @@
+<template>
+	<div>
+
+		<draggable v-model="icons" item-key="id" class="flex flex-col mx-auto space-y-4">
+			<template #item="{element}">
+				<div class="icon" v-html="element.svg"></div>
+			</template>
+			<template #footer>
+				<div>
+					click to add
+					<div v-for="icon in availableIcons" class="icon"><span v-html="icon.svg" @click="add(icon.id)"></span></div>
+				</div>
+			</template>
+		</draggable>
+
+	</div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+let id = 1;
+export default {
+	name: 'DME App',
+	components: {
+		draggable
+	},
+	data() {
+		return {
+			icons: [],
+			availableIcons: [
+				{
+					name: "John",
+					id: 0,
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="icon-svg" viewBox="0 0 16 16"><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/> </svg>'
+				},
+				{
+					name: "John",
+					id: 1,
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="icon-svg" viewBox="0 0 16 16"><path d="M9 5a.5.5 0 0 0-1 0v3H6a.5.5 0 0 0 0 1h2.5a.5.5 0 0 0 .5-.5V5z"/><path d="M4 1.667v.383A2.5 2.5 0 0 0 2 4.5v7a2.5 2.5 0 0 0 2 2.45v.383C4 15.253 4.746 16 5.667 16h4.666c.92 0 1.667-.746 1.667-1.667v-.383a2.5 2.5 0 0 0 2-2.45V8h.5a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5H14v-.5a2.5 2.5 0 0 0-2-2.45v-.383C12 .747 11.254 0 10.333 0H5.667C4.747 0 4 .746 4 1.667zM4.5 3h7A1.5 1.5 0 0 1 13 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 11.5v-7A1.5 1.5 0 0 1 4.5 3z"/></svg>'
+				},
+				{
+					name: "John",
+					id: 2,
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="icon-svg" viewBox="0 0 16 16"><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/> </svg>'
+				},
+				{
+					name: "John",
+					id: 3,
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="icon-svg" viewBox="0 0 16 16"><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/> </svg>'
+				},
+				{
+					name: "John",
+					id: 4,
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="icon-svg" viewBox="0 0 16 16"><path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/> </svg>'
+				}
+			],
+			dragging: false,
+			componentData: {
+				type: "transition",
+				name: "flip-list"
+			}
+		};
+	},
+	methods: {
+		add: function (id) {
+			let index = this.availableIcons.findIndex(item => item.id === id );
+			console.log(index);
+			this.icons.push(
+			    this.availableIcons[index]
+			);
+		},
+		clear() {
+			this.icons = [];
+		}
+	},
+
+	computed: {
+		dragOptions() {
+			return {
+				animation: 0,
+				group: "description",
+				disabled: false,
+				ghostClass: "ghost"
+			};
+		}
+	}
+}
+</script>
+
+
